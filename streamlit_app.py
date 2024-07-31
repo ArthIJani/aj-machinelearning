eb449c42dae8301cdb661789aaa7b56ce09315a2
Arth I Jani <85545030+ArthIJani@users.noreply.github.com>
2024-07-31 10:19:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,3 +20,18 @@ with st.expander('Data'):
 
 with st.expander('Data Visualization'):
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
+
+#Data Preparations
+with st.sidebar:
+  st.header('Input Features')
+  island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
+  gender = st.selectbox('Gender', ('Male', 'Female'))
+  bill_length_mm = st.slider('Bill Length (mm)', 32.1, 59.6, 43.9)
+  #bill_depth_mm = 
+  #flipper_length_mm = 
+  #body_mass_g = 
+
+
+
+
+
